518926a189d204d635986e981fcd5010b85ba6a8
Katerina Komarkova <katerina.komarkova@hotmail.com>
2021-05-23 12:55:13 +0200

    custom css individual-movie-container

--- BlazorMovies/Shared/Entities/Movies.cs
+++ BlazorMovies/Shared/Entities/Movies.cs
@@ -10,5 +10,6 @@ namespace BlazorMovies.Shared.Entities
     {
         public string Title { get; set; }
         public DateTime ReleaseDate { get; set; }
+        public string Poster { get; set; }
     }
 }

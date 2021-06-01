5c88bb98704b95fca0f001eaaea8c0c1527424fb
Katerina Komarkova <katerina.komarkova@hotmail.com>
2021-06-01 14:59:39 +0200

    customize error  validation message

--- BlazorMovies/Shared/Entities/Genre.cs
+++ BlazorMovies/Shared/Entities/Genre.cs
@@ -10,7 +10,7 @@ namespace BlazorMovies.Shared.Entities
     public class Genre
     {
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage ="This field is required")]
         public string Name { get; set; }
     }
 }

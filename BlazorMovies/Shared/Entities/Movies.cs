4d6c4486870b004a0523752c6ddf8f4d247498cc
Katerina Komarkova <katerina.komarkova@hotmail.com>
2021-06-03 16:07:20 +0200

    Movie form

--- BlazorMovies/Shared/Entities/Movies.cs
+++ BlazorMovies/Shared/Entities/Movies.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +9,14 @@ namespace BlazorMovies.Shared.Entities
 {
     public class Movie
     {
-        public int Id { get; set; } = 1;
+        public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
         public string Trailer { get; set; }
         public string Summary { get; set; }
         public bool InTheaters { get; set; }
-        public DateTime ReleaseDate { get; set; }
+        [Required]
+        public DateTime? ReleaseDate { get; set; }
         public string Poster { get; set; }
         public string TitleBrief 
         {

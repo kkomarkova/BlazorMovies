9436eefad6b38ea230741d11736ab5464f2eac75
Katerina Komarkova <katerina.komarkova@hotmail.com>
2021-06-03 11:34:01 +0200

    markdown component adding

--- BlazorMovies/Shared/Entities/Movies.cs
+++ BlazorMovies/Shared/Entities/Movies.cs
@@ -10,6 +10,9 @@ namespace BlazorMovies.Shared.Entities
     {
         public int Id { get; set; } = 1;
         public string Title { get; set; }
+        public string Trailer { get; set; }
+        public string Summary { get; set; }
+        public bool InTheaters { get; set; }
         public DateTime ReleaseDate { get; set; }
         public string Poster { get; set; }
         public string TitleBrief 
